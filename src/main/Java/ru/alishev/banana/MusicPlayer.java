b4e428ca55b85cee10de36a752823ff7522db991
trachemys-scripta57 <6416417@gmail.com>
2022-09-05 17:51:15 +0300

    1.Создать 3й класс - жанр музыки; 2.MusicPlayer хранит список музыкальных жанров не отдельными полями, а списком list; 3.Создаём бин для нового жанра музки; 4.Создаём бин-список муз.жанров; 5.Метод playMusic() воспроизводит песню случайного муз жанра (класс Random); 6.@Component, @Autowired и @ComponentScan не используем - всё вручную в конфиг.файле.

--- src/main/Java/ru/alishev/banana/MusicPlayer.java
+++ src/main/Java/ru/alishev/banana/MusicPlayer.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MusicPlayer {
     @Value("${musicPlayer.name}")
     private String name;
@@ -23,7 +23,7 @@ public class MusicPlayer {
     private Music music1;
     private Music music2;
 
-    @Autowired
+//    @Autowired
     public MusicPlayer(@Qualifier("rockMusic") Music music1,
                        @Qualifier("classicalMusic") Music music2) {
         this.music1 = music1;
--- src/main/Java/ru/alishev/banana/TestSpring.java
+++ src/main/Java/ru/alishev/banana/TestSpring.java
@@ -1,11 +1,14 @@
 package ru.alishev.banana;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.annotation.Annotation;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
