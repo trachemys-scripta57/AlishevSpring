a09a94989e3f411a8e9bfa1aea51ca2c37a7feb3
trachemys-scripta57 <6416417@gmail.com>
2022-09-12 12:06:34 +0300

    Пересоздал проект "Spring MVC. Первое приложение" В предыдущей версии на любые изменения выдавало страницу Hello World! ... В данной (вновь созданной) версии - выдаёт только пустую страницу.. (:

---
+++ src/main/java/ru/banana/mvc/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package ru.banana.mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null; // пока не будем использовать
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
+// класс заменяет web.xml
