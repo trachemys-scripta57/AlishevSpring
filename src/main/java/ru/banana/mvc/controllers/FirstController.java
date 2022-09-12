bab10639c4cc5043c24eace476ff8800a7519921
trachemys-scripta57 <6416417@gmail.com>
2022-09-12 19:07:00 +0300

    3.21 Передаём параметры через Model в представление и отобразим их

--- src/main/java/ru/banana/mvc/controllers/FirstController.java
+++ src/main/java/ru/banana/mvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.banana.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,29 +9,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/first") // меняет путь с http://localhost:8080/goodbye на
-//http://localhost:8080/first/goodbye ...ломает все ранее прописанные пути
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-//    public String helloPage(HttpServletRequest request) {
-//        /* внедряем через Spring объект в котором содержатся все
-//        сведения от Пользователя в виде http-запроса */
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
-//        /* получаем из объекта параметры запроса */
-
-
     /* доступ к параметрам через аннотацию */
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-/* в отличии от HttpServletRequest запрос без параметров выдаст ошибку
-* required = false убирает эту ошибку*/
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        /* Передаём параметры через Model в представление и отобразим их */
 
-        System.out.println("Hello, "+name +" "+surname);
+//        System.out.println("Hello, "+name +" "+surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+        /* помещаем строку  "Hello, "+name +" "+surname в model. Ключ (любой) - message*/
 
-        return"first/hello";
-}
+        return "first/hello";
+    }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
