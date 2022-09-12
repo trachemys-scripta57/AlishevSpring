d145b373dc1e59bcc40147a5b619c1562b53edc0
trachemys-scripta57 <6416417@gmail.com>
2022-09-12 18:43:50 +0300

    3.20 GET запрос. Получение параметров из объекта

--- src/main/java/ru/banana/mvc/controllers/FirstController.java
+++ src/main/java/ru/banana/mvc/controllers/FirstController.java
@@ -3,16 +3,34 @@ package ru.banana.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping("/first") // меняет путь с http://localhost:8080/goodbye на
+@RequestMapping("/first") // меняет путь с http://localhost:8080/goodbye на
 //http://localhost:8080/first/goodbye ...ломает все ранее прописанные пути
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
-        return "first/hello";
-    }
+//    public String helloPage(HttpServletRequest request) {
+//        /* внедряем через Spring объект в котором содержатся все
+//        сведения от Пользователя в виде http-запроса */
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        /* получаем из объекта параметры запроса */
+
+
+    /* доступ к параметрам через аннотацию */
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+/* в отличии от HttpServletRequest запрос без параметров выдаст ошибку
+* required = false убирает эту ошибку*/
+
+        System.out.println("Hello, "+name +" "+surname);
+
+        return"first/hello";
+}
 
     @GetMapping("/goodbye")
     public String goodByePage() {
