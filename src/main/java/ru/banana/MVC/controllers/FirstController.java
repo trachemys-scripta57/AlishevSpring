08fb5586e288c51284e5d9950e321600c6c9efa3
trachemys-scripta57 <6416417@gmail.com>
2022-09-14 10:00:19 +0300

    Восстановлена работа Tomcat 10, artefact WarExploded...

--- src/main/java/ru/banana/MVC/controllers/FirstController.java
+++ src/main/java/ru/banana/MVC/controllers/FirstController.java
@@ -1,4 +1,4 @@
-package ru.banana.MVC.config.controllers;
+package ru.banana.MVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
