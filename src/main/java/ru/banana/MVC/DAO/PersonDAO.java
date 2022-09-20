b4171dcf47397d64a5b9da69234139a0547f10bc
trachemys-scripta57 <6416417@gmail.com>
2022-09-20 12:04:53 +0300

    Урок 3.27 SQL injection. PreparedStatement

--- src/main/java/ru/banana/MVC/DAO/PersonDAO.java
+++ src/main/java/ru/banana/MVC/DAO/PersonDAO.java
@@ -57,43 +57,72 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
-//        return people.stream().filter(person -> person.getId() == id)
-//                .findAny().orElse(null);
-///* находим человека по id и если такого нет, возвращаем null */
-        return null;
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setEmail(resultSet.getString("email"));
+            person.setAge(resultSet.getInt("age"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return person;
     }
 
     public void save(Person person) {
-//        person.setId(++PEOPLE_COUNT);
-//        people.add(person);
-
         try {
-            Statement statement = connection.createStatement();
-            String SQL = "insert into person values(" + 1 + ", " + "'" + person.getName()
-                    + "', " + person.getAge() + ", '" + person.getEmail() + "')";
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("insert into Person VALUES(1, ?, ?, ?)");
 
-            //INSERT INTO Person VALUES(1, 'Tom', 18, 'aaa@aa.ru')
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
 
-            statement.executeUpdate(SQL);
-            //executeUpdate -изменяет данные в БД
-
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void update(int id, Person updatedPerson) {
-//        Person personToBeUpdated = show(id);
-////получаем новые данные edit.html и выполняем замену
-//        personToBeUpdated.setName(updatedPerson.getName());
-//        personToBeUpdated.setAge(updatedPerson.getAge());
-//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
+
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void delete(int id) {
-//        people.removeIf(p -> p.getId() == id);
-        //проходим по каждому человеку из списка, получаем его id
-        //removeIf метод ArrayList
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement =
+                    connection.prepareStatement("DELETE FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 //обычно общается с БД. Но для примера строим коллекцию
