a0a9b4f6757467621d9d5083170ace6f5cceeafe
trachemys-scripta57 <6416417@gmail.com>
2022-09-21 11:44:50 +0300

    Урок 4.31 Автоматическая генерация id при создании новой строки

--- src/main/java/ru/banana/MVC/DAO/PersonDAO.java
+++ src/main/java/ru/banana/MVC/DAO/PersonDAO.java
@@ -31,7 +31,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(),
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
                 person.getEmail());
     }
 
